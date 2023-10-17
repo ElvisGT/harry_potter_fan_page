fb155794508dfc22cfb8a4a0cad708dcd635f4e2
Elvis <elvisgt1999@gmail.com>
2023-10-17 19:32:49 +0200

    Terminado el componente de buscar por nombre

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import { useFetchCharacters } from '../hooks/useFetchCharacters';
 import { Ring } from '@uiball/loaders'
 import CharactersCard from './CharactersCard';
+import { useSearchCharacter } from '../hooks/useSearchCharacter';
+import SearchCharacter from './SearchCharacter';
 
 export const App = () => {
   const {data,loading} = useFetchCharacters();
+  const characterRef = useRef("");
+  const {findedCharacters,handleSearch} = useSearchCharacter(data,characterRef)
 
   return (
     <>
         <h1 className='title'>Bienvenido al mundo de los Simpsons</h1>
+        
+        {/*Componente Buscador*/}
+        <SearchCharacter characterRef = {characterRef} handleSearch={handleSearch}/>
+
         {loading && 
           <div className="ring">
             <Ring 
@@ -16,12 +24,11 @@ export const App = () => {
             lineWeight={5}
             speed={2} 
             color="black" 
-            
             />
           </div>
         }
         {
-          data.map(item => (
+          findedCharacters.map(item => (
             <CharactersCard key={item.id} {...item}/>
           ))
         }
